5d7d6ef380390c6088f0389e074165e973c07ce6
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-27 18:27:00 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    routes: {
+      '/': (context) => const Placeholder(),
+      '/semesters': (context) => const Placeholder(),
+      '/calculator': (context) => const Placeholder(),
+      '/results': (context) => const Placeholder(),
+      '/criteria': (context) => const Placeholder(),
+    }
+  ));
+}
