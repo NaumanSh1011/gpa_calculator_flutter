ffc677db08fdf1880caca41b1325588769d60b28
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-27 18:33:48 +0500

    Added Widgets for different pages

---
+++ lib/Pages/semesters.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Semesters extends StatefulWidget {
+  const Semesters({Key? key}) : super(key: key);
+
+  @override
+  State<Semesters> createState() => _SemestersState();
+}
+
+class _SemestersState extends State<Semesters> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gpa_calculator/Pages/home.dart';
+import 'package:gpa_calculator/Pages/semesters.dart';
+import 'package:gpa_calculator/Pages/results.dart';
+import 'package:gpa_calculator/Pages/calculator.dart';
 
 void main() {
   runApp(MaterialApp(
     routes: {
-      '/': (context) => const Placeholder(),
-      '/semesters': (context) => const Placeholder(),
-      '/calculator': (context) => const Placeholder(),
-      '/results': (context) => const Placeholder(),
+      '/': (context) => const Home(),
+      '/semesters': (context) => const Semesters(),
+      '/calculator': (context) => const Results(),
+      '/results': (context) => const Calculator(),
       '/criteria': (context) => const Placeholder(),
     }
   ));
