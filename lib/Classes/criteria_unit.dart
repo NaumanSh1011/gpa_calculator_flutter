b6cfb7b7e9b7642dad5205ebfc6cfc5da2d61171
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-03-02 20:08:24 +0500

    Added criteria_unit.dart to handle criteria of grading

---
+++ lib/Classes/criteria_unit.dart
@@ -0,0 +1,23 @@
+class CriteriaUnit{
+  String lowerLimit;
+  String gradePoints;
+  bool isDistributed;
+
+  CriteriaUnit({required this.lowerLimit, required this.gradePoints, required this.isDistributed});
+
+  factory CriteriaUnit.fromJson(Map<String, dynamic> json) {
+    return CriteriaUnit(
+      lowerLimit: json['lowerLimit'],
+      gradePoints: json['gradePoints'],
+      isDistributed: json['isDistributed'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'lowerLimit': lowerLimit,
+      'gradePoints': gradePoints,
+      'isDistributed': isDistributed,
+    };
+  }
+}
