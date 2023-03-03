5b10ab6d78f816a1b19539d2c85bcaa3ee43b8f9
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-03-03 21:27:19 +0500

    Fixed UI bugs

--- lib/Pages/criteria.dart
+++ lib/Pages/criteria.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gpa_calculator/Classes/criteria_unit.dart';
 import 'package:gpa_calculator/Widgets/criteria_widget.dart';
 import 'dart:convert';
@@ -15,11 +16,15 @@ class Criteria extends StatefulWidget {
 class _CriteriaState extends State<Criteria> {
   List<CriteriaUnit> criteria = [];
   bool isDistribute = false;
+  TextEditingController lowerLimitController = TextEditingController(text: "");
+  TextEditingController gradePointController = TextEditingController(text: "");
+  final focusNode1 = FocusNode();
+  final focusNode2 = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController lowerLimitController = TextEditingController(text: "");
-    TextEditingController gradePointController = TextEditingController(text: "");
+    focusNode1.unfocus();
+    focusNode2.unfocus();
     return Scaffold(
       appBar: AppBar(
         title: const Text('GPA Criteria'),
@@ -38,36 +43,44 @@ class _CriteriaState extends State<Criteria> {
             ),
             const SizedBox(height: 10),
             Expanded(
-              child: ListView.builder(
-                itemCount: criteria.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    enabled: false,
-                    onTap: () {},
-                    title: CriteriaWidget(
-                      index: index,
-                      gradeController: List.generate(
-                        criteria.length,
-                            (index) => TextEditingController(),
-                        growable: true,
+              child: Container(
+                color: Colors.grey[300],
+                child: ListView.builder(
+                  itemCount: criteria.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                        child: ListTile(
+                          enabled: false,
+                          onTap: () {},
+                          title: CriteriaWidget(
+                            index: index,
+                            gradeController: List.generate(
+                              criteria.length,
+                                  (index) => TextEditingController(),
+                              growable: true,
+                            ),
+                            limitController: List.generate(
+                              criteria.length,
+                                  (index) => TextEditingController(),
+                              growable: true,
+                            ),
+                            criteria: criteria,
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                criteria.removeAt(index);
+                              });
+                            },
+                            icon: const Icon(Icons.delete),
+                          )
+                        ),
                       ),
-                      limitController: List.generate(
-                        criteria.length,
-                            (index) => TextEditingController(),
-                        growable: true,
-                      ),
-                      criteria: criteria,
-                    ),
-                    trailing: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          criteria.removeAt(index);
-                        });
-                      },
-                      icon: const Icon(Icons.delete),
-                    )
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
             Row(
@@ -79,18 +92,26 @@ class _CriteriaState extends State<Criteria> {
                   child: Column(
                     children: [
                       TextField(
+                        focusNode: focusNode1,
                         decoration: const InputDecoration(
                           border: UnderlineInputBorder(),
                           hintText: 'Lower Limit',
                         ),
                         controller: lowerLimitController,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                       ),
                       TextField(
+                        focusNode: focusNode2,
                         decoration: const InputDecoration(
                           border: UnderlineInputBorder(),
                           hintText: 'Grade Point',
                         ),
                         controller: gradePointController,
+                        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
+                        ],
                       ),
                     ],
                   ),
@@ -124,6 +145,16 @@ class _CriteriaState extends State<Criteria> {
 
                                 setState(() {
                                   isDistribute = false;
+                                  lowerLimitController.text = "";
+                                  gradePointController.text = "";
+                                  // Check which text field is currently focused
+                                  if (focusNode1.hasFocus) {
+                                    // Remove the focus from the first text field
+                                    focusNode1.unfocus();
+                                  } else if (focusNode2.hasFocus) {
+                                    // Remove the focus from the second text field
+                                    focusNode2.unfocus();
+                                  }
                                   criteria.add(cu);
                                 });
                               },
